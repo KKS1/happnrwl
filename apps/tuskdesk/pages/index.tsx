08287ccff826d74419acc2589131a5bde35dfd92
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-01-21 10:10:43 -0800

    .

--- apps/tuskdesk/pages/index.tsx
+++ apps/tuskdesk/pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from './index.module.css';
 
-import { SharedUiLayout } from '@happynrwl/ui-components';
+import { SharedUiLayout } from '@happnrwl/ui-components';
 
 export function Index() {
   /*
